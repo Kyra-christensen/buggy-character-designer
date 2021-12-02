bf3d71146ee6e47a5d2f9439fc26116ca6dff5c1
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-02 14:33:49 -0800

    fixed some bugs

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const headDropdown = document.getElementById('head-dropdown');
+import { makeStatsString } from './utils.js';
+const headDropdown = document.getElementById('head-form');
 const middleDropdown = document.getElementById('middle-dropdown');
 const bottomDropdown = document.getElementById('bottom-dropdown');
-const headEl = document.getElementById('top');
+const headEl = document.getElementById('head');
+const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
 const reportEl = document.getElementById('report');
-const chatchphrasesEl = document.getElementById('chatch-phrases');
+const chatchphrasesEl = document.getElementById('chatchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
+const catchphraseButton = document.getElementById('catchphrase-button');
 
-const headCount = 0;
-const middleCount = 0;
-const bottomCount = 0;
+console.log(headDropdown);
+console.log(middleDropdown);
+console.log(bottomDropdown);
+console.log(headEl);
+console.log(middleEl);
+console.log(bottomEl);
+console.log(reportEl);
+console.log(chatchphrasesEl);
+console.log(catchphraseButton);
+console.log(catchphraseInput);
 
-const catchphrases = 0;
+
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
+
+let catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
-    const value = headDropdown.value;
+    const id = headDropdown.value;
 
     headCount++;
-    headEl.backgroundImage = `url("./assets/${value}-head.png")`;
+
+    headEl.backgroundImage = `url('./assets/${id}-head.png')`;
     displayStats();
 });
 
@@ -48,15 +64,18 @@ catchphraseButton.addEventListener = () => {
 };
 
 function displayStats() {
-    return `You have changed the head ${headCount} times, the body ${middleCount} times, and the pants ${bottomCount} times. And nobody can forget your character's classic catchphrases:`;
+    const statsString = makeStatsString(headCount, middleCount, bottomCount);
+    reportEl.textContent = statsString;
 }
 
 function displayCatchphrases() {
+    chatchphrasesEl.textContent = '';
+
     for (let catchphrase of catchphrases) {
         const p = document.createElement('p');
 
-        p.add('catchphrase');
-        p.textContent = catchphrases;
+        p.classList.add('catchphrase');
+        p.textContent = catchphrase;
 
         chatchphrasesEl.append(p);
     }
